9b5bf1dee170ddc148823cdb231e59cbf0ffee16
Shawkik-Islam-Joha <shawkikislam@gmail.com>
2025-01-15 17:35:17 +0600

    new lines added

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # LEARNING-OOP
+Here I will add all the codes I wrote during the learning process of Object Oriented Programming with C++.
--- intro_to_c_plus_plus.cpp
+++ intro_to_c_plus_plus.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#define newl "\n"
+
 using namespace std;
 
 int counter = 100; //Declaring Global variable
@@ -8,5 +10,6 @@ int main(){
     int counter = 5;
     cout << "Value of local variable (counter) is: " << counter << endl; //access the local variable
     cout << "Value of global variable (counter) is: " << ::counter << endl; //access the global variable
+    cout << "Hi Joha!" << newl << "How you doin?" << newl;
     return 0;
 }
