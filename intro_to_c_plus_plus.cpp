fc419b1471657680ecb67b17e336c61736b1a3dd
Shawkik-Islam-Joha <shawkikislam@gmail.com>
2025-01-15 18:31:32 +0600

    nothin

--- intro_to_c_plus_plus.cpp
+++ intro_to_c_plus_plus.cpp
@@ -12,5 +12,9 @@ int main(){
     cout << "Value of global variable (counter) is: " << ::counter << endl; //access the global variable
     cout << "Hi Joha!" << newl << "How you doin?" << newl;
     for(int i=0;i<100;i++)cout << i << newl;
+    int x;
+    cin >> x;
+    if(x&1)cout << "Odd" << newl;
+    else cout << "Even" << newl;
     return 0;
 }
