484199b459df68eb74ec7ff559902522d495dc5f
SHAWKIK ISLAM JOHA <shawkikislam@gmail.com>
2025-03-10 17:24:35 +0600

    Modifications

--- Lecture_3/1_Assigning_Objects.cpp
+++ Lecture_3/1_Assigning_Objects.cpp
@@ -2,6 +2,14 @@
 
 using namespace std;
 
+class teacher{
+    string name,course;
+    public:
+    teacher(string a="XYZ",string b = "EEE - XYZ"){
+        name = a,course = b;
+    }
+}t1;
+
 class student{
     public:
     string id;
@@ -18,6 +26,8 @@ int main(){
     cout << "Student 3 Id = "  << s3.id << " CGPA = " << s3.cgpa << endl;
 
     s1 = s3;    //Assigniung s3 data to s1,techincally bitwise copy pasting.
+
+//  s2 = t1;    //attempting to assign different data type will throw an error
 
     cout << "Student 1 Id = "  << s1.id << " CGPA = " << s1.cgpa << endl;
 
---
+++ Lecture_3/2_Memory_Allocation.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+class learn{
+    int *a; //declared pointer variable
+    public:
+    learn(int x){
+        a = (int*)malloc(sizeof(int));
+        //malloc dynamically creates a space of size 4 in memory and return that adress as void pointer,then we cast it to int pointer.
+        if(!a){
+            cout << "Memory Allocatioon failure!" << endl;
+            exit(true); //terminates program
+        }
+        *a = x;
+    }
+    ~learn(){
+        free(a);
+        cout << "Freeing memory" << endl;
+    }
+    int get(){
+        return *a;
+    }    
+};
+
+int main(){
+    int y = 120;
+    int *p; //Declaring an integer pointer;
+    p = &y;
+    cout << p << endl;  //prints the memory location
+    cout << *p << endl; //prints the value stored in that location
+
+    learn l1(10),l2(50);
+    cout << l1.get() << endl;
+    cout << l2.get() << endl;
+    return 0;
+}
