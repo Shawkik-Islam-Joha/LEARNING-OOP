8a5c5a561417a94cc77c1671cc90cc9a5ddb4b3f
SHAWKIK ISLAM JOHA <shawkikislam@gmail.com>
2025-03-11 15:58:13 +0600

    Updated

--- Lecture_4/1_Array_Of_Objects.cpp
+++ Lecture_4/1_Array_Of_Objects.cpp
@@ -13,12 +13,31 @@ public:
     }
 };
 
+class book{
+public:
+    int price;
+public:
+    book(int x=150){
+        price = x;
+    }
+};
+
 int main(){
     student eee[5]={student("2202124",3.75),student("2202126",3.8),student("2202164",3.8)};
     //Generated an array of objects of class student and initialised them.
     for(int i=0;i<5;i++){
     //Running loop to access data from objects.
         cout << "Student Id - " << eee[i].id << " CGPA - " << eee[i].cgpa << endl;
+    }
+
+    cout << "_________________________________" << endl;
+    
+    book bb[10] = {10,300,450,500};
+    //book bb[10] = {book(10),book(300),book(450)};
+    
+    for(int i=0;i<10;i++){
+        //Running loop to access data from objects.
+            cout << "Book - " << i+1 << " Price - " << bb[i].price << endl;
     }
     return 0;
 }
